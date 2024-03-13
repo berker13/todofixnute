360eace009a9d7e865e76840a7a46254718e83f9
Berker <vojta.janota13@gmail.com>
2024-03-13 12:00:05 +0100

    Initial commit

---
+++ nextjs/src/app/api/todo/route.tsx
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request, res: Response){
+  const todos = await prisma.todo.findMany();
+
+  if(!todos){
+    return NextResponse.json("failed")
+  }
+  return NextResponse.json(todos, {status: 200});
+}
+
+export async function POST(req: Request, res: Response){
+  const params = await req.json();
+  const todos = await prisma.todo.create({
+    data: {
+      title: params,
+    },
+  });
+  
+  if(!todos){
+    return NextResponse.json("failed")
+  }
+
+  revalidatePath("/")
+  return NextResponse.json(todos, {status: 200});
+}
---
+++ nextjs/src/lib/prisma.ts
@@ -0,0 +1,31 @@
+"use server"
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+const prisma = new PrismaClient();
+
+export async function getTodos(){
+    const todos = await prisma.todo.findMany({})
+    if(!todos) return null;
+    return todos;
+}
+
+export async function addTodo(title: string){
+    const todo = await prisma.todo.create({
+        data:{
+            title: title.toString(),
+        },
+    });
+    if(!todo) return null;
+    revalidatePath("/")
+}
+
+export async function delTodo(id: number){
+    const todo = await prisma.todo.delete({
+        where:{
+            id: id,
+        },
+    });
+    if(!todo) return null;
+    revalidatePath("/")
+}
